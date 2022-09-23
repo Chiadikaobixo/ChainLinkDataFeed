75d2d6df7db8139df5ba9372b9e853464434f674
chiadikaobixo <chiadikaobixo@gmail.com>
2022-09-23 17:23:11 -0500

    modified datafeed contract

--- contracts/DataFeed.sol
+++ contracts/DataFeed.sol
@@ -4,14 +4,20 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract DataFeed {
-    AggregatorV3Interface internal ethereum;
+    AggregatorV3Interface internal ETH2USD;
+    AggregatorV3Interface internal BTC2ETH;
+    AggregatorV3Interface internal BTC2USD;
     
     /**
      * @dev Initializes the Goerli Testnet proxy 
-     * `0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e` for the ETH / USD
+     * for the ETH / USD
+     * BTC / ETH
+     * BTC / USD
     */
     constructor() {
-        ethereum = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        ETH2USD = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        BTC2ETH=AggregatorV3Interface(0x779877A7B0D9E8603169DdbD7836e478b4624789);
+        BTC2USD=AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7);
     }
     
     /**
@@ -24,7 +30,38 @@ contract DataFeed {
            ,
            ,
            
-        ) = ethereum.latestRoundData();
+        ) = ETH2USD.latestRoundData();
+        // scale down the answer by 10 ** 8 in order to get price in USD
+        $ = answer / 1e8;
+        return $;
+    }
+    /**
+     @dev returns the price of BTC to Ethereum
+    */
+    function getBTC2ETH() public view returns (int $){
+        (
+           ,
+           int256 answer,
+           ,
+           ,
+           
+        ) = BTC2ETH.latestRoundData();
+        // scale down the answer by 10 ** 18 in order to get price in ETH
+        $ = answer / 1e18;
+        return $;
+    }
+
+    /**
+     @dev returns the price of ethereum to usd
+    */
+    function getBTC2USD() public view returns (int $){
+        (
+           ,
+           int256 answer,
+           ,
+           ,
+           
+        ) = BTC2USD.latestRoundData();
         // scale down the answer by 10 ** 8 in order to get price in USD
         $ = answer / 1e8;
         return $;
